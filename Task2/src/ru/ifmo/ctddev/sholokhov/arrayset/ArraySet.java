e8a3a327716feeffa447aa25fefe54f0766b93de
Alex Sholokhov <ak.sholokhov@gmail.com>
2015-02-25 02:46:40 +0300

    task2 in progress (without testing)

---
+++ Task2/src/ru/ifmo/ctddev/sholokhov/arrayset/ArraySet.java
@@ -0,0 +1,111 @@
+package ru.ifmo.ctddev.sholokhov.arrayset;
+
+import java.util.*;
+
+/**
+ * Created by Шолохов on 24.02.2015.
+ */
+public class ArraySet<T> extends AbstractSet<T> implements SortedSet<T> {
+
+    private ArrayList<T> data;
+    private Comparator<? super T> comparator;
+
+    private class MyComparator<T> implements Comparator<T> {
+        @Override
+        public int compare(T o1, T o2) {
+            return ((Comparable<T>) o1).compareTo(o2);
+        }
+    }
+
+    private class MyIterator<T> implements Iterator<T> {
+        Iterator<T> c;
+
+        public MyIterator(Iterator<T> c) {
+            this.c = c;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return c.hasNext();
+        }
+
+        @Override
+        public T next() {
+            return c.next();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public ArraySet(Collection<T> data) {
+        ArrayList<T> data2 = new ArrayList<T>(data);
+        Collections.sort(data2, new MyComparator());
+        this.data = new ArrayList<T>(data);
+        this.comparator = new MyComparator<T>();
+    }
+
+    public ArraySet(Collection<T> data, Comparator<? super T> comparator) {
+        ArrayList<T> data2 = new ArrayList<T>(data);
+        Collections.sort(data2, comparator);
+        this.data = new ArrayList<T>(data2);
+        this.comparator = comparator;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyIterator<T>(data.iterator());
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+
+    @Override
+    public Comparator<? super T> comparator() {
+        return comparator;
+    }
+
+    @Override
+    public SortedSet<T> subSet(T fromElement, T toElement) {
+        return new ArraySet<T>(data.subList(Collections.binarySearch(data, fromElement, comparator), Collections.binarySearch(data, toElement, comparator)), comparator);
+    }
+
+    @Override
+    public SortedSet<T> headSet(T toElement) {
+        return new ArraySet<T>(data.subList(0, Collections.binarySearch(data, toElement, comparator)), comparator);
+    }
+
+    @Override
+    public SortedSet<T> tailSet(T fromElement) {
+        return new ArraySet<T>(data.subList(Collections.binarySearch(data, fromElement, comparator), data.size()), comparator);
+    }
+
+    @Override
+    public T first() {
+        return data.get(0);
+    }
+
+    @Override
+    public T last() {
+        return data.get(data.size() - 1);
+    }
+
+    @Override
+    public boolean add(T t) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ Task2/src/ru/ifmo/ctddev/sholokhov/arrayset/Main.java
@@ -0,0 +1,31 @@
+package ru.ifmo.ctddev.sholokhov.arrayset;
+
+import ru.ifmo.ctddev.sholokhov.arrayset.ArraySet;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ * Created by Шолохов on 24.02.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> t = new ArrayList<Integer>();
+        t.add(1);
+        t.add(5);
+        t.add(3);
+        t.add(2);
+
+        ArraySet<Integer> test = new ArraySet<Integer>(t, Comparator.<Integer>naturalOrder());
+        test.forEach((x) -> System.out.print(x));
+        System.out.println('\n' + "size = " + test.size());
+        test.subSet(1, 1).forEach((x) -> System.out.print(x));
+        System.out.println("");
+        test.tailSet(2).forEach((x) -> System.out.print(x));
+        System.out.println("");
+        test.headSet(5).forEach((x) -> System.out.print(x));
+
+        ArrayList<Integer> t2 = new ArrayList<Integer>();
+        test = new ArraySet<Integer>(t);
+    }
+}
